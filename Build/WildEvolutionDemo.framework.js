86f9815927ec6ca2a61735827abd88d2ea23f1af
ledzzsos <ledzzb620@gmail.com>
2026-09-22 08:30:14 +0800

    Publish WebGL demo from 698b21e

--- Build/WildEvolutionDemo.framework.js
+++ Build/WildEvolutionDemo.framework.js
@@ -1993,13 +1993,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  1772224: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 1772285: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 1772349: function() {return Module.webglContextAttributes.powerPreference;},  
- 1772407: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 1772462: function($0) {performance.now = function() { return $0; };},  
- 1772510: function($0) {performance.now = function() { return $0; };},  
- 1772558: function() {performance.now = Module['emscripten_get_now_backup'];}
+  1772336: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 1772397: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 1772461: function() {return Module.webglContextAttributes.powerPreference;},  
+ 1772519: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 1772574: function($0) {performance.now = function() { return $0; };},  
+ 1772622: function($0) {performance.now = function() { return $0; };},  
+ 1772670: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
